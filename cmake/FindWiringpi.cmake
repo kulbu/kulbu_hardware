ad2e94193ff65ae6212d9da8201e5c40981014a8
Mr-Yellow <mr-yellow@mr-yellow.com>
2015-06-06 19:51:05 +0930

    Target wiringPi buils

--- cmake/FindWiringpi.cmake
+++ cmake/FindWiringpi.cmake
@@ -1,17 +1,18 @@
 # - Find wiringPi
-#
-#  wiringpi_INCLUDES  - List of wiringPi includes
-#  wiringpi_FOUND     - True if wiringPi found.
 
 # Look for the header file.
 find_path(wiringPi_INCLUDE NAMES wiringPi.h
-  PATHS $ENV{WIRINGPI_ROOT} /usr /usr/local /usr/share)
+  PATHS $ENV{WIRINGPI_ROOT}/include /usr /usr/local /usr/share)
+
+find_library(wiringPi_LIBRARY NAMES wiringPi
+  PATHS $ENV{WIRINGPI_ROOT}/lib /usr/lib /usr/local/lib)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(wiringPi DEFAULT_MSG wiringPi_INCLUDE)
+find_package_handle_standard_args(wiringPi DEFAULT_MSG wiringPi_INCLUDE wiringPi_LIBRARY)
 
-if(wiringPi_FOUND)
-  message(STATUS "Found wiringPi (include: ${wiringPi_INCLUDE})")
-  set(wiringPi_INCLUDES ${wiringPi_INCLUDE})
-  mark_as_advanced(wiringPi_INCLUDE)
-endif()
+# if(wiringPi_FOUND)
+#  message(STATUS "Found wiringPi (include: ${wiringPi_INCLUDE}, ${wiringPi_LIBRARY})")
+#  set(wiringPi_INCLUDES ${wiringPi_INCLUDE})
+#  set(wiringPi_LIBRARIES ${wiringPi_LIBRARY})
+#  mark_as_advanced(wiringPi_INCLUDE wiringPi_LIBRARY)
+#endif()
